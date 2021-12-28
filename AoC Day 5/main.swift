a664f9dc98e0bbd423edc9fd4b8da34c6fb3fa83
Moritz Diedenhofen <moritzdiedenhofen@googlemail.com>
2021-12-28 20:26:15 +0100

    Solution Day 5 Part 1

--- AoC Day 5/main.swift
+++ AoC Day 5/main.swift
@@ -8,25 +8,27 @@ func loadData() -> String{
     }
 }
 
-let data = loadData().split(separator: "\n")
-
-let coordinates = data.map {coordinate(rawvalue: String($0))}
-
+let coordinates = loadData().split(separator: "\n").map {coordinate(rawvalue: String($0))}
 var playGround = [[Int]]()
+var playgroundSize = 1000
 
-for _ in 0 ... 999 {
-    
-    var field = [Int](repeating: 0, count: 999)
-    playGround.append(field)
+for _ in 0 ... playgroundSize {
+    playGround.append([Int](repeating: 0, count: playgroundSize))
 }
 
 var overlapingCount = 0
 let overlapingSpecification = 2
+
 for cor in coordinates {
     DrawLine(coordinate: cor, OnlyWhenIdentical: true)
 }
 
-printPlayground()
+for i in 0 ... playgroundSize - 1{
+    for ii in 0 ... playgroundSize - 1 {
+        if playGround[i][ii] >= 2 {overlapingCount += 1}
+    }
+}
+
 print(overlapingCount)
 
 func DrawLine(coordinate : coordinate, OnlyWhenIdentical: Bool) {
@@ -34,13 +36,8 @@ func DrawLine(coordinate : coordinate, OnlyWhenIdentical: Bool) {
     if OnlyWhenIdentical && !(coordinate.xIsIdentical() || coordinate.yIsIdentical()) { return }
     
     for y in coordinate.yFrom ... coordinate.yTo {
-        
         for x in coordinate.xFrom ... coordinate.xTo {
-     
             playGround[y][x] += 1
-            if (playGround[y][x] >= overlapingSpecification) {
-                overlapingCount += 1
-            }
         }
     }
 }
@@ -48,13 +45,13 @@ func DrawLine(coordinate : coordinate, OnlyWhenIdentical: Bool) {
 
 
 func printPlayground() {
-    print("##############")
+
     for row in playGround {
         var line = ""
         for clm in row {
             if (clm == 0) {
                 line.append(".")
-            }else {
+            } else {
                 line.append(String(clm))
             }
             
@@ -63,8 +60,6 @@ func printPlayground() {
     }
     
 }
-
-
 
 struct coordinate {
     
